7da1ace412045f3f4a5894d32a9b042fea55bdfd
Alejandro Mendez Rojas <amr@Alejandros-MacBook-Pro.local>
2024-02-20 18:21:15 -0600

    Front end session finished. It could be possible to add detection for multiple faces

--- src/App.js
+++ src/App.js
@@ -1,124 +1,60 @@
 import React from 'react';
+import ParticlesBg from 'particles-bg';
+import MouseParticles from 'react-mouse-particles' //mine 
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Register from './components/Register/Register';
 import './App.css';
 import 'tachyons';
-import ParticlesBg from 'particles-bg';
-import MouseParticles from 'react-mouse-particles' //mine 
+
 //https://github.com/lindelof/react-mouse-particles
 
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <MouseParticles g={1} color="random" cull="col,image-wrapper"/> {/*Mine*/}
-//       <ParticlesBg className='particles' type="circle" bg={true} />
-//       <Navigation />
-//       <Logo />
-//       <Rank />
-//       <ImageLinkForm />
-//       {/*
-            
-//             <FaceRecognition />*/}
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: ''
-    }
-  }
-
-  onInputChange = (event) => {
-    console.log(event.target.value);
-  }
-
-  onButtonSubmit = () => {
-    console.log('click');
-  }
-
-  render (){
-    return (
-      <div className="App">
-        <MouseParticles g={1} color="random" cull="col,image-wrapper"/> {/*Mine*/}
-        <ParticlesBg className='particles' type="circle" bg={true} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-        onInputChange={this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit} 
-        />
-        {/*
-              
-              <FaceRecognition />*/}
-      </div>
-    )
-  };
+const clarifaiJSONRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = 'dddddd4366314e48ab070884a87e40f4';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'rif01';
+  const APP_ID = 'facebrain';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                    // "base64": IMAGE_BYTES_STRING
+                }
+            }
+        }
+      ]
+  });
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };  
+  return requestOptions
 }
 
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// In this section, we set the user authentication, user and app ID, model details, and the URL
-// of the image we want as an input. Change these strings to run your own example.
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-// // Your PAT (Personal Access Token) can be found in the portal under Authentification
-// const PAT = 'dddddd4366314e48ab070884a87e40f4';
-// // Specify the correct user_id/app_id pairings
-// // Since you're making inferences outside your app's scope
-// const USER_ID = 'clarifai';
-// const APP_ID = 'main';
-// // Change these to whatever model and image URL you want to use
-// const MODEL_ID = 'face-detection';
-// const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-// const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
-// // To use image bytes, assign its variable   
-// // const IMAGE_BYTES_STRING = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAAoACgDASIAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAAYDBQcE/8QAMBAAAQMDAwMDAgQHAAAAAAAAAQIDBAAFEQYSIQcTMTJBURRhCBYikSNScXKhsdH/xAAZAQACAwEAAAAAAAAAAAAAAAAFBgIDBAf/xAAtEQABAwMBBgQHAQAAAAAAAAABAgMRAAQhMQUSE0FRYQaBocEUFiJCcrHR8P/aAAwDAQACEQMRAD8A3+RYY1unSYzCS0ttZUkAgktn0q5yT7jPyDUC4wdGwycH5U2Kt9ZQ7VI1qw5PkvQy3CSVPpf7aQjuKyFH25xzn3pHn3TVNy01Hl2hyy6YdkSpKsS9sl/6RlI3rRu3dxWd6spwnAGPIJTfl925fcLaoSDHXvyo6i9SlCQrU9wKln3OyWiaDN1RAbW3kKbSd7gPtwMkH/tTWy9afuy1iPfnXMAblITwkE4yf08cn3pSbYt1uts24XH6fUbiLAuY1MWyGkLEmUW0rcCRvUpQ5CtwKQCPgi4S1ZbDe4sd9NntDEe79m3uOBLTr0IR9jzodSMqUpTu9JJ8owD7UTT4ZCfv9PbP7860m+s+HBSrejWRuz2kAxoesGYxTW/Zlpkwo1vkuSly3UgKWQUhHJUvIHsAaKTemF8XE6sWmxyZkiaZrMh1jv8ArQNpUVqB8FW0njHqx4zRVVhsph1KlKk5xQ+7uHmikaSJrQerMByet2IwvtuTLa4xv2k7Rk84H9x/esHv92d01boenLXGcuiWrFIhLlpbcaQ2/JdK3VJCkAq2pAR7Zz7YxWudY9fxNIdQbNGkR5TyX4aisNNpUMFZAzkj4NK0jq9ZpbLr0PSlzkhrlZDaQlP3P8Q4/ap3F87bPucJEkx/hHv60b2TYXLrKN5sramYECSQRk9M6c6zmJ+eb5Hi22M7cnWGIQgFLbX0zSo4PDa1YBcTgDyMjJ/qbGPabH08SJt1Uzc9QqRliGg5QySPKvgc+TyfYDmmTUWpNYz7ctxoQdPQshCktupckDJUPUcJT6DwMq8YyaQ9VL0pCS8zapcq4SVOBZmPDO8/cnknlWcDBwn4NYnPjLkQ+qE9OtOVlYpeVHDCEkkkJyT+SuQzy5Y0ru6Ez511/Efa5s1fdkOtyVurIxgdlQAA9gOKKPwolU7remU5hCGYEgo38KUv9I/0TRTDYJCWQBSF4rIN/CRgAR0iTpVD1j1g/qDqJcJqlKcjB9bcda142MpOEJAzgeMnjyTSyze5KEuNRpDoDvC0oe4X9iAeaKKFK+oya6fbOqYbDTeEiAPKpHdS3gBLYc7RQkp3ApQog+cq8nwPJrljzxnPZbUfnugn/NFFRgEVch9xKsH0H8pg6e3x3T3UC1ajaZITGkJLoS4MKbOUrzz/ACKVRRRVzVwtoQmhG1NkWu0HuI+JI8u/Kv/Z';
-
-// ///////////////////////////////////////////////////////////////////////////////////
-// // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-// ///////////////////////////////////////////////////////////////////////////////////
-
-// const raw = JSON.stringify({
-//     "user_app_id": {
-//         "user_id": USER_ID,
-//         "app_id": APP_ID
-//     },
-//     "inputs": [
-//         {
-//             "data": {
-//                 "image": {
-//                     "url": IMAGE_URL
-//                     // "base64": IMAGE_BYTES_STRING
-//                 }
-//             }
-//         }
-//     ]
-// });
-
-// const requestOptions = {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Authorization': 'Key ' + PAT
-//     },
-//     body: raw
-// };
-
-// // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-// // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-// // this will default to the latest version_id
-
-// fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+// fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", clarifaiJSONRequestOptions(this.state.input))
 //     .then(response => response.json())
 //     .then(result => {
 
@@ -144,6 +80,94 @@ class App extends React.Component {
 
 //     })
 //     .catch(error => console.log('error', error));
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width); 
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", clarifaiJSONRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log);
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignIn: false})
+    }else if (route === 'home') {
+      this.setState({isSignIn: true})
+    }
+    this.setState({route: route});
+  }
+
+// video 288 for corrections
+
+  render (){
+    const { isSignIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <MouseParticles g={1} color="random" cull="col,image-wrapper"/> {/*Mine*/}
+        <ParticlesBg className='particles' type="circle" bg={true} />
+        <Navigation 
+          isSignIn={isSignIn}
+          onRouteChange={this.onRouteChange} 
+        
+        />
+        { route === 'home' 
+          ?<div> 
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+              onInputChange={this.onInputChange} 
+              onButtonSubmit={this.onButtonSubmit} 
+              />
+              <FaceRecognition 
+                box={box} 
+                imageUrl={imageUrl} />
+            </div>
+          : route === 'register'
+          ? <Register onRouteChange={this.onRouteChange}/>
+          : <Signin onRouteChange={this.onRouteChange}/>
+          
+          }
+      </div>
+    )
+  };
+}
+
 
 
 export default App;
